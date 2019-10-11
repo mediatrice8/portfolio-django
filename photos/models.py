f3b50cb03eb43885bf4205553e77e34ae036c3a7
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-11 15:05:37 +0200

    class for location

--- photos/models.py
+++ photos/models.py
@@ -3,8 +3,9 @@ from django.db import models
 # Create your models here.
 
 
-class Picture(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
-    technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+class Location(models.Model):
+    location_name = models.CharField(max_length=50, unique=True)
+    
+    # def __str__(self):
+    #     return self.location_name
+
