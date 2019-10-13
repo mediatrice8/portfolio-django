b67511460dc0c6bc3bda5bec869dfa22008237a0
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-14 00:50:44 +0200

    dashboard for admin

---
+++ photos/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Location,Category,Image
+# Register your models here.
+
+
+# Register your models here.
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Image)
