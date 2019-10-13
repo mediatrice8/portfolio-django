b1438b0b486304a1134711055e69b7e418f3c58d
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-14 00:47:45 +0200

    routes inside of app

---
+++ photos/urls.py
@@ -0,0 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.conf.urls import url
+from . import views
+
+urlpatterns=[
+    url('^$',views.welcome,name = 'welcome'),
+    url('^$',views.index,name='index'),
+    url(r'^search/',views.search_results, name='search_results'),
+    url(r'^category/(\w+)', views.categoryPage,name='categoryPage'),
+    url(r'^location/(\w+)', views.locationPage,name='locationPage'),
+]
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
