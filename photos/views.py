69874de5ef90cc185ff9ffbd10a385edf43954a7
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-14 00:46:20 +0200

    function or method used by classes

---
+++ photos/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from .models import Location,Category,Image
+
+
+# Create your views here.
+def welcome(request):
+    return render(request, 'welcome.html')
+
+def index(request):
+    images = Image.objects.all()
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Home'
+
+    return render(request,'welcome.html', {'images':images,'locations':locations,'categories':categories, 'title':title})
+
+
+def search_results(request):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = 'Search'
+
+    if 'pictures' in request.GET and request.GET["pictures"]:
+        search_term = request.GET.get("pictures")
+        searched_images = Image.search_category(search_term)
+        message = f"Results for: {search_term}"
+
+        return render(request, 'welcome.html',{"message":message,"images": searched_images,'locations':locations,'categories':categories, 'title':title})
+
+    else:
+        message = "You haven't searched for any things."
+        return render(request, 'welcome.html' ,{"message":message})
+def categoryPage(request,category):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = f"{category}"
+    category_results = Image.search_category(category)
+    return render(request,'welcome.html',{'images':category_results,'locations':locations,'categories':categories, 'title':title})
+
+def locationPage(request,location):
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    title = f"{location}"
+    location_results = Image.filter_location(location)
+    return render(request,'welcome.html',{'images':location_results,'locations':locations,'categories':categories, 'title':title})
