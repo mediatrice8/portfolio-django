0cb0fee618b2d95173a175a9632185195713cba4
mediatrice8 <kampiremediatrice@gmail.com>
2019-10-14 15:15:05 +0200

    update the app

--- photos/urls.py
+++ photos/urls.py
@@ -2,13 +2,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^$',views.index,name='index'),
+    # url('^$',views.index,name='index'),
     url(r'^search/',views.search_results, name='search_results'),
     url(r'^category/(\w+)', views.categoryPage,name='categoryPage'),
     url(r'^location/(\w+)', views.locationPage,name='locationPage'),
 ]
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
